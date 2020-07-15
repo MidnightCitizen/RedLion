39893e900b6a7521498bf3831b1f64b298b37392
MidnightCitizen <themidnightcitizen@hotmail.fr>
2020-07-15 21:18:32 +0200

    log to understand

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 const main = async function main() {
        // loading credentials and document
        console.log('Before the credentials loading');
+       console.log('The value of the key :');
+       console.log(process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'));
        await doc.useServiceAccountAuth({
               client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
               private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
